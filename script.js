74b7e40308e6c245c8f99e35e81e563b8947047f
sophie <sophie1620@gmail.com>
2022-03-05 21:07:09 -0500

    started media queries and finished js for header and form

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const sophie = {};
 
 sophie.init = function() {
-    sophie.form()
+    sophie.form();
+
+    sophie.h2Element();
 } //end of init function
 
+
+
+// delay h2 entrance
+sophie.h2Element = () => {
+    const h2Element = document.createElement('h2');
+    h2Element.textContent = `I'm Sophie!`;
+
+    h2Element.classList.add('animate__animated', 'animate__fadeInLeft');
+
+    setTimeout( () => {
+        const h2div = document.querySelector('.h2div');
+        h2div.appendChild(h2Element);
+
+    }, 3500);
+}
 
 // form submit event listener
 sophie.form = () => {
@@ -17,12 +34,17 @@ sophie.form = () => {
         // console.log(name.value);
         const senderName = name.value;
         console.log('senderName:', senderName);
+        const senderGreeting = senderName.toUpperCase();
 
         fieldset.innerHTML = "";
 
         const pElement = document.createElement('p');
-        pElement.textContent = `Thank you for sending Sophie an email, ${senderName}! She will get back to you as soon as possible. ☺️`
+        pElement.textContent = `Thank you for sending Sophie an email, ${senderGreeting}!`;
         fieldset.append(pElement);
+
+        const p2Element = document.createElement('p');
+        p2Element.textContent = 'She will get back to you as soon as possible. ☺️';
+        fieldset.append(p2Element);
 
     })
 }
